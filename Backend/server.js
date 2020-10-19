b3932e32aeb5160a60de22583188d00d1ab68260
YoussefAlashmawy <36681247+JOoAlashmawy@users.noreply.github.com>
2020-10-19 16:35:01 +0200

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+// var knex = require('knex')({
+//   client: 'mysql',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'your_database_user',
+//     password : 'your_database_password',
+//     database : 'myapp_test'
+//   }
+// });
+// OR 
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'joo',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'john',
+      password: 'cookies',
+      email: 'john@gmail.com',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'weak',
+      password: 'cookie',
+      email: 'weak@gmail.com',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+  login: [
+    {
+      hash: '',
+      id: '',
+      email: '',
+    },
+  ],
+};
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id:'125',
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length-1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+    if (!found) {
+      res.status(400).json(user.entries);
+    }
+  });
+});
+
+app.listen('3000', () => {
+  console.log('app is running on port 3000');
+});
